1ab37fc149e2f87cf6ad4ca4fcef1dc5be39a4c2
AbdulsemedM <abdumussema22@gmail.com>
2025-02-05 23:38:47 +0300

    chnage

--- lib/configuration/api_constants.dart
+++ lib/configuration/api_constants.dart
@@ -1,5 +1,6 @@
 class ApiConstants {
-  static const String baseUrl = 'http://10.8.100.45:8080';
+  // static const String baseUrl = 'http://10.8.100.45:8080';
+  static const String baseUrl = 'https://10.101.200.211:8080';
   // Future<void> fetchData() async {
   //   final apiProvider = ProviderSetup.getApiProvider(
   //     baseUrl,
